b3be7e8691f1a5b12fb514b9135dc33315923146
torre <torre@DESKTOP-O3QDVPL>
2021-09-12 22:57:52 +0200

    Ferdig

--- Java innleveringer/src/Javaobl1/Oppgave1.java
+++ Java innleveringer/src/Javaobl1/Oppgave1.java
@@ -1,5 +1,31 @@
 package Javaobl1;
 
+import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.*;
+
 public class Oppgave1 {
+
+	public static void main(String[] args) {
+		
+			int inntekt = parseInt(showInputDialog("Skriv inntekt"));
+			
+			double netto1 = inntekt+(inntekt*0.017);
+			double netto2 = inntekt-(inntekt*0.04);
+			double netto3 = inntekt-(inntekt*0.132);
+			double netto4 = inntekt-(inntekt*0.162);
+		
+			
+			if (inntekt <= 184800)
+				showMessageDialog(null,"Du er i Trinn 0 og betaler ingen Trinnskatt");
+			else if (inntekt <= 260100)
+				showMessageDialog(null, "Du er i Trinn 1 og betaler 1,7% Trinnskatt"+"Før skatt; "+inntekt+ " Etter Skatt: " + netto1);
+			else if (inntekt <= 651250)
+				showMessageDialog(null, "Du er i Trinn 2 og betaler 4,0% Trinnskatt"+"Før skatt; "+inntekt+ " Etter Skatt: " + netto2);
+			else if (inntekt <= 1021500)
+				showMessageDialog(null, "Du er i Trinn 3 og betaler 13,2% Trinnskatt"+"Før skatt; "+inntekt+ " Etter Skatt: " + netto3);
+			else if (inntekt > 1021500)
+				showMessageDialog(null, "Du er i Trinn 4 og betaler 16,2% Trinnskatt "+"Før skatt; "+inntekt+ " Etter Skatt: " + netto4);
+				
+	}
 
 }
--- Java innleveringer/src/Javaobl1/Oppgave2.java
+++ Java innleveringer/src/Javaobl1/Oppgave2.java
@@ -1,5 +1,29 @@
 package Javaobl1;
+import static java.lang.Integer.*;
+import static javax.swing.JOptionPane.*;
 
-public class Oppgave2 {
+	public class Oppgave2 {
+		
+	public static void main(String[] args) {
+		for(int in=1; in<=10; in++) {
+			int poeng = parseInt(showInputDialog("Poeng på prøve "+ in ));	
+			if(poeng<0 ||poeng>100){
+	        showMessageDialog(null,"Feil");
+	        in -=1;
+			}
+			else if(poeng < 39 )
+				showMessageDialog(null,poeng+" poeng gir karakter F");
+			else if (poeng <= 49 )
+				showMessageDialog(null,poeng+" poeng gir karakter E");
+			else if (poeng <= 59)
+				showMessageDialog(null,poeng+" poeng gir karakter D");
+			else if (poeng <=79)
+				showMessageDialog(null,poeng+" poeng gir karakter C");
+			else if (poeng <=89)
+				showMessageDialog(null,poeng+" poeng gir karakter B");
+			else if (poeng <=100)
+				showMessageDialog(null,poeng+" poeng gir karakter A");	
+	    }
+		}
 
 }
